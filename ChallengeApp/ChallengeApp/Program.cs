e93e145477c89e455f89a8468b10db6dce3eff90
michalparysz <michalparysz@gmx.de>
2023-11-18 09:25:40 +0100

    Zadanie Dzień 4

--- ChallengeApp/ChallengeApp/Program.cs
+++ ChallengeApp/ChallengeApp/Program.cs
@@ -1,2 +1,9 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿var Age = 20;
+string Name = "Maja";
+string Gender = "Male";
+string result = Name + Gender + Age;
+Console.WriteLine(Name + Gender + Age);
+if (Age < 30)
+    Console.WriteLine("Kobietaa poniżej 30lat");
+if (Age > 18)
+    Console.WriteLine("Pełnoletnia kobieta");
